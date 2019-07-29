e3d95adf6d7dc38d70700d92a80ee6340a876483
renyalvarado <renyalvarado@gmail.com>
2019-07-28 23:35:11 -0300

    Add lookup_entry_by_name testing method

--- test_phonebook.py
+++ test_phonebook.py
@@ -6,6 +6,11 @@ class PhonebookCase(unittest.TestCase):
     def test_create_phonebook(self):
         phonebook = Phonebook()
 
+    def test_lookup_entry_by_name(self):
+        phonebook = Phonebook()
+        phonebook.add("Bob", "12345")
+        self.assertEqual("12345", phonebook.lookup("Bob"))
+
 
 if __name__ == '__main__':
     unittest.main()
